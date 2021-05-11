4fffecc6c396d12939bcdd61716bb4e16ba5723a
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-05-11 12:50:49 +0200

    feat: printStatus method using a enum for the diferent status codes

---
+++ src/main/java/edu/pingpong/mordorcommand/Status.java
@@ -0,0 +1,5 @@
+package edu.pingpong.mordorcommand;
+
+public enum Status {
+    ACCEPTED, REJECTED;
+}
--- src/main/java/edu/pingpong/mordorcommand/processors/Office.java
+++ src/main/java/edu/pingpong/mordorcommand/processors/Office.java
@@ -1,5 +1,7 @@
 package edu.pingpong.mordorcommand.processors;
 
+import edu.pingpong.mordorcommand.Status;
+import edu.pingpong.mordorcommand.interfaces.Order;
 import edu.pingpong.mordorcommand.interfaces.OrderTreatment;
 import edu.pingpong.mordorcommand.interfaces.Processor;
 
@@ -8,5 +10,11 @@ public class Office implements Processor {
     @Override
     public boolean processes(OrderTreatment orderTreatment) {
         return orderTreatment.treat();
+    }
+
+    // Using Ternary operator (condition? true result : false result)
+    public String printStatus(boolean status, Order order) {
+        return status? order.getDestination() + " " + Status.ACCEPTED.name() :
+                order.getDestination() + " " + Status.REJECTED.name();
     }
 }
